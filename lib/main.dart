c2a1dd26c872fa16adcd7b9c774cf151e5e07325
Raz Levi <razle102030@gmail.com>
2021-11-16 15:58:19 +0200

    Add Project Skeleton
    Issue #2

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+// ================= App =================
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const BalanceMeApp());
+}
+
+class BalanceMeApp extends StatelessWidget {
+  const BalanceMeApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(),
+    );
+  }
+}
