8d0faba82251782e859c4d65df59f652077d5760
Tzahi Levi <tazachil@gmail.com>
2021-12-08 20:59:10 +0200

    Update generic_radio_button.dart
    Fixed as the requested in the PR by @Raz-Levi
    
    issue #110

--- lib/widgets/generic_radio_button.dart
+++ lib/widgets/generic_radio_button.dart
@@ -1,40 +1,47 @@
 // ================= Generic Radio Button =================
+import 'package:balance_me/global/types.dart';
 import 'package:flutter/material.dart';
 import 'package:balance_me/global/constants.dart' as gc;
 
 /*
-* The widget receives a List of String:
-* ["hello", "world", ... ]
+* The widget receives a List of String: ["hello", "world", ... ], controller of type PrimitiveWrapper(String value),
+* initial String from the provided List, and callBack function.
 * The widgets presents a row of radio buttons with the strings in the list.
+* The initial selected button is the one which is provided as _initialValue.
+* in order to use the PrimitiveWrapper first create variable for example:
+* PrimitiveWrapper controller = PrimitiveWrapper("");
+* And then pass it to the constructor of this widget:
+* GenericRadioButton(..., controller, ... , ...);
+* The value of the selected radio button stored at the _radioButtonController.value.
+* The callBack function is called when you change selection.
 * The order of the buttons is the same order as the list.
 * If the list is too long the widget will continue in the next line.
 */
 
 class GenericRadioButton extends StatefulWidget {
-  GenericRadioButton(this._options, this._radioButtonController,
-      {this.onTapCallback, Key? key})
-      : super(key: key);
+  const GenericRadioButton(this._options, this._radioButtonController,this._initialValue, {this.onTapCallback, Key? key}) : super(key: key);
+
   final List<String> _options;
-  late String _radioButtonController;
+  final PrimitiveWrapper _radioButtonController;
+  final String _initialValue;
   final VoidCallback? onTapCallback;
-
-
-  set radioButtonController(String value) {
-    _radioButtonController = value;
-  }
 
   @override
   State<GenericRadioButton> createState() => _GenericRadioButtonState();
 }
 
 class _GenericRadioButtonState extends State<GenericRadioButton> {
-
-  void _activateRadioButton(String? value){
-    setState(() {
-      widget._radioButtonController = value!;
-      widget.onTapCallback;
-    });
+  void _activateRadioButton(String? value) {
+    if (value != null) {
+      setState(() {
+        widget._radioButtonController.value = value;
+        if (widget.onTapCallback != null) {
+          widget.onTapCallback!();
+        }
+      });
+    }
   }
+
   List<Widget> _radioButtonOptions() {
     List<Widget> buttonOptions = [];
     for (String data in widget._options) {
@@ -45,16 +52,25 @@ class _GenericRadioButtonState extends State<GenericRadioButton> {
               Radio<String>(
                 activeColor: gc.primaryColor,
                 value: data,
-                groupValue: widget._radioButtonController,
+                groupValue: widget._radioButtonController.value,
                 onChanged: (String? value) => _activateRadioButton(value),
               ),
-              Text(data)
-            ],
-          ));
+              Text(data),
+        ],
+      ));
     }
     return buttonOptions;
   }
 
+  @override
+  void initState(){
+    if (widget._options.contains(widget._initialValue)) {
+      widget._radioButtonController.value = widget._initialValue;
+    }else {
+      throw Exception("The _initialValue is not in the list");
+    }
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
     return Wrap(
@@ -62,5 +78,4 @@ class _GenericRadioButtonState extends State<GenericRadioButton> {
       children: _radioButtonOptions(),
     );
   }
-
 }
