3596b140c68acfc32f9301d30510f49f20d545ed
Raz Levi <razle102030@gmail.com>
2021-12-18 19:38:48 +0200

    Make editButton stateless

--- lib/widgets/generic_edit_button.dart
+++ lib/widgets/generic_edit_button.dart
@@ -2,25 +2,19 @@
 import 'package:flutter/material.dart';
 import 'package:balance_me/global/constants.dart' as gc;
 
-class GenericIconButton extends StatefulWidget {
+class GenericIconButton extends StatelessWidget {
   const GenericIconButton({Key? key, this.onTap, this.color, this.iconSize}) : super(key: key);
 
   final GestureTapCallback? onTap;
   final Color? color;
   final double? iconSize;
 
-  @override
-  _GenericIconButtonState createState() => _GenericIconButtonState();
-}
-
-class _GenericIconButtonState extends State<GenericIconButton> {
-  @override
   Widget build(BuildContext context) {
     return IconButton(
-      onPressed: widget.onTap,
+      onPressed: onTap,
       disabledColor: gc.disabledColor.withOpacity(0.0),
-      iconSize: widget.iconSize ?? gc.editIconSize,
-      color: widget.color ?? gc.alternativePrimary,
+      iconSize: iconSize ?? gc.editIconSize,
+      color: color ?? gc.alternativePrimary,
       icon: const Icon(gc.editIcon),
     );
   }
