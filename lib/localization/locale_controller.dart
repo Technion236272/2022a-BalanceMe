63f1c1ba31368fc7b107de8c63a645403c445a9d
Raz Levi <razle102030@gmail.com>
2021-12-19 23:45:36 +0200

    Fix using device language
    Issue #115

--- lib/localization/locale_controller.dart
+++ lib/localization/locale_controller.dart
@@ -1,20 +1,12 @@
 // ================= Locale Controller Functions =================
-import 'package:balance_me/main.dart';
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:balance_me/main.dart';
 import 'package:balance_me/firebase_wrapper/google_analytics_repository.dart';
 import 'package:balance_me/localization/languages_controller.dart';
 import 'dart:io';
-import 'package:balance_me/global/constants.dart' as gc;
-
-Future<Locale> setLocale(String languageCode) async {
-  SharedPreferences _prefs = await SharedPreferences.getInstance();
-  await _prefs.setString(gc.prefSelectedLanguageCode, languageCode);
-  return _locale(languageCode);
-}
 
 Future<Locale> getLocale() async {
-  String languageCode = Platform.localeName.split('_')[0];
+  String languageCode = Platform.localeName.split('_')[0];  // device language
   if (!LanguageController().supportedLanguageCodesList().contains(languageCode)) {
     languageCode = LanguageController().defaultLanguage.languageCode;
   }
@@ -26,8 +18,7 @@ Locale _locale(String languageCode) {
 }
 
 void changeLanguage(BuildContext context, String selectedLanguageCode) async {
-  Locale _locale = await setLocale(selectedLanguageCode);
-  BalanceMeApp.setLocale(context, _locale);
+  BalanceMeApp.setLocale(context, _locale(selectedLanguageCode));
   GoogleAnalytics.instance.logChangeLanguage(selectedLanguageCode);
 }
 
