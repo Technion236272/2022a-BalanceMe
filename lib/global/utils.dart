76b7eae9598cbcd441adf5e0d922b92f3d0b934f
Raz Levi <razle102030@gmail.com>
2021-12-25 23:47:56 +0200

    Bug Fix- remove sign from text fields

--- lib/global/utils.dart
+++ lib/global/utils.dart
@@ -101,16 +101,6 @@ extension En on Enum {
   String toCleanString() => toString().split(".")[1];
 }
 
-class CurrencyFormatter extends TextInputFormatter{
-  CurrencyFormatter(this.currencySign);
-  final String currencySign;
-
-  @override
-  TextEditingValue formatEditUpdate(TextEditingValue oldValue, TextEditingValue newValue) {
-    return newValue.copyWith(text: newValue.text + currencySign);
-  }
-}
-
 // Validators
 bool essentialFieldValidator(String? value) => (value != null && value.isNotEmpty);
 
