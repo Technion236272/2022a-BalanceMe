a591d8459077cb6c3c385010dd3a1d05baab0521
Dalia Chulsky <62931065+arch-dimension@users.noreply.github.com>
2022-01-03 17:06:11 +0200

    issue #214
    set states added to update and delete
    loading spinner shows up, but only briefly, and after reload

--- lib/widgets/user_avatar.dart
+++ lib/widgets/user_avatar.dart
@@ -2,27 +2,46 @@
 import 'package:flutter/material.dart';
 import 'package:balance_me/firebase_wrapper/auth_repository.dart';
 import 'package:balance_me/global/constants.dart' as gc;
-
-class UserAvatar extends StatelessWidget {
-  const UserAvatar(this._authRepository, this._radius, {Key? key}) : super(key: key);
+class UserAvatar extends StatefulWidget {
+  const UserAvatar(this._authRepository, this._radius,
+      {Key? key, this.proportion})
+      : super(key: key);
 
   final AuthRepository _authRepository;
   final double _radius;
+  final double? proportion;
 
   @override
+  State<UserAvatar> createState() => _UserAvatarState();
+}
+
+class _UserAvatarState extends State<UserAvatar> {
+  @override
   Widget build(BuildContext context) {
-    return _authRepository.avatarUrl != null ?
-    Padding(
-      padding: gc.settingAppbarAvatarPadding,
-      child: CircleAvatar(
-        backgroundImage: NetworkImage(_authRepository.avatarUrl!),
-        radius: _radius,
-        backgroundColor: Colors.transparent,
-      ),
-    )
-    : const Icon(
-        gc.emptyAvatarIcon,
-        size: gc.settingDefaultAppbarAvatarSize
-    );
+    return widget._authRepository.avatarUrl != null
+        ? Padding(
+            padding: gc.settingAppbarAvatarPadding,
+            child: CircleAvatar(
+              radius: widget._radius,
+              backgroundColor: Colors.transparent,
+              child: ClipOval(
+                  child:
+                  Image.network(
+                widget._authRepository.avatarUrl!,
+                fit: BoxFit.cover,
+                width: widget.proportion ?? widget._radius,
+                height: widget.proportion ?? widget._radius,
+                loadingBuilder: (BuildContext context, Widget child,
+                    ImageChunkEvent? loadingProgress) {
+                  if (loadingProgress == null) {
+                    return child;
+                  }
+                  return CircularProgressIndicator();
+                },
+              )),
+            ),
+          )
+        : const Icon(gc.emptyAvatarIcon,
+            size: gc.settingDefaultAppbarAvatarSize);
   }
 }
