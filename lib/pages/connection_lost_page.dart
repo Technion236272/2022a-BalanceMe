d37e1d44c6958d911982ffa7986b031382b6cc1c
Raz Levi <razle102030@gmail.com>
2022-01-06 18:56:28 +0200

    Determine connection lost

---
+++ lib/pages/connection_lost_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ConnectionLostPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // https://github.com/abuanwar072/20-Error-States-Flutter
+    return Scaffold(
+        body: Text("no internet"),
+    );
+  }
+}
