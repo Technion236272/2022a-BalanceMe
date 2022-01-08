9d671ac216bc4df6ef3a5174f933114670d88b10
Tzahi Levi <tazachil@gmail.com>
2022-01-08 20:37:01 +0200

    Update generic_tooltip.dart
    - Addition of TextStyle option to the GenericTooltip

--- lib/widgets/generic_tooltip.dart
+++ lib/widgets/generic_tooltip.dart
@@ -2,8 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:balance_me/global/constants.dart' as gc;
 
 class GenericTooltip extends StatefulWidget {
-  const GenericTooltip({Key? key, this.tip}) : super(key: key);
+  const GenericTooltip({Key? key, this.tip, this.style}) : super(key: key);
   final String? tip;
+  final TextStyle style;
 
   @override
   _GenericTooltipState createState() => _GenericTooltipState();
@@ -14,6 +15,7 @@ class _GenericTooltipState extends State<GenericTooltip> {
   Widget build(BuildContext context) {
     return Tooltip(
       message: widget.tip,
+      textStyle: widget.style,
       child: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Icon(gc.detailsIcon),
