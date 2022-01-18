a0125c1c2c71def5b453b4dc0bbf9e4b01bcd6bd
Raz Levi <razle102030@gmail.com>
2022-01-18 13:37:24 +0200

    Revert "Revert "Integrate FireBase Performance Monitoring""
    This reverts commit 0d22cd869331acb94ea55d01406b97f858495314.

--- lib/main.dart
+++ lib/main.dart
@@ -9,12 +9,11 @@ import 'package:balance_me/localization/resources/resources.dart';
 import 'package:balance_me/firebase_wrapper/auth_repository.dart';
 import 'package:balance_me/firebase_wrapper/storage_repository.dart';
 import 'package:balance_me/firebase_wrapper/google_analytics_repository.dart';
+import 'package:firebase_performance/firebase_performance.dart';
 import 'package:sentry_flutter/sentry_flutter.dart';
 import 'package:balance_me/pages/home.dart';
 import 'package:balance_me/global/types.dart';
 import 'package:balance_me/global/config.dart' as config;
-import 'package:balance_me/global/constants.dart' as gc;
-
 import 'controllers/theme_controller.dart';
 
 Future<void> main() async {
@@ -29,6 +28,7 @@ Future<void> main() async {
 
 class App extends StatelessWidget {
   final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+  FirebasePerformance performance = FirebasePerformance.instance;
 
   App({Key? key}) : super(key: key);
 
